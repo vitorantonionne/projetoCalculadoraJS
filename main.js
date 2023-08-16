5c0c10869eb6162207facfc59065302cf7ebadef
Vitor Antonionne <114239671+vitorantonionne@users.noreply.github.com>
2023-08-16 19:36:34 -0300

    funcionalidade

--- main.js
+++ main.js
@@ -10,3 +10,23 @@ document.querySelectorAll(".charKey").forEach(function (charKeyBtn) {
     input.value += value
   })
 })
+
+document.getElementById("clear").addEventListener("click", function () {
+  input.value = ""
+  input.focus()
+})
+
+input.addEventListener("keydown", function (ev) {
+  ev.preventDefault()
+  if (allowedKeys.includes(ev.key)) {
+    input.value += ev.key
+    return
+  }
+  if (ev.key === "Backspace") {
+    input.value = input.value.slice(0, -1)
+  }
+  if (ev.key === "Enter") {
+    calculate()
+  }
+})
+
