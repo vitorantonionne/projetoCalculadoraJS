91badbd7b9f0084f0a004397d0bb9626b33d736b
Vitor Antonionne <114239671+vitorantonionne@users.noreply.github.com>
2023-08-16 19:35:16 -0300

    init

---
+++ main.js
@@ -0,0 +1,12 @@
+const main = document.querySelector("main")
+const root = document.querySelector(":root")
+const input = document.getElementById("input")
+const resultInput = document.getElementById("result")
+const allowedKeys = ["(", ")", "/", "*", "-", "+", "9", "8", "7", "6", "5", "4", "3", "2", "1", "0", ".", "%", " "]
+
+document.querySelectorAll(".charKey").forEach(function (charKeyBtn) {
+  charKeyBtn.addEventListener("click", function () {
+    const value = charKeyBtn.dataset.value
+    input.value += value
+  })
+})
